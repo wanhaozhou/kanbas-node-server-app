2ce2f550c755ccdf1dc096770cfb17fe20095eb8
Wanhao Zhou <wanhao.zhou@gmail.com>
2023-11-09 15:36:52 -0500

    a5: minor

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import ModuleRoutes from "./modules/routes.js";
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000'
 }));
 app.use(express.json());
 
